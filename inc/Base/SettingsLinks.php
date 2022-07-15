38a79d36defa75f1a161dedcfcf35da62b2744a7
navidabdi <artsanaat@gmail.com>
2022-07-15 17:18:39 +0300

    add SettingLinks class

---
+++ inc/Base/SettingsLinks.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace WebkimaElements\Base;
+
+
+class SettingsLinks
+{
+    public function register()
+    {
+        add_filter('plugin_action_links_' . WEBKIMA_ELEMENTS_NAME, array($this, 'settings_links'));
+    }
+
+    public function settings_links($links)
+    {
+        $setting_links = '<a href="admin.php?page=webkima_elements">Settings</a>';
+        array_push($links, $setting_links);
+        return $links;
+    }
+
+}
