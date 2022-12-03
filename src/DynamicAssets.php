c00e3e32aa3d2cc5580879ca8c86427be4fbed16
Nabi <artsanaat@gmail.com>
2022-12-03 19:02:05 +0300

    Added CssMinifire class.

--- src/DynamicAssets.php
+++ src/DynamicAssets.php
@@ -7,12 +7,25 @@
  */
 
 namespace WebkimaElements;
+use WebkimaElements\CssMinifier;
 
 class DynamicAssets {
 
   public static array $styles = [];
 
   public static function printStyles(): array {
+	  $fileNames  = [
+			WEBKIMA_ELEMENTS_PATH . 'assets/css/iranyekan-font.css',
+		  WEBKIMA_ELEMENTS_PATH . 'assets/css/vazir-font.css',
+	  ];
+
+	  $myfile = fopen(WEBKIMA_ELEMENTS_PATH . 'assets/css/main.css', "w") or die("Unable to open file!");
+//	  $txt = self::$styles['gotoup'];
+	  $cssMinifier = new CssMinifier($fileNames);
+	  $txt = $cssMinifier->minify();
+	  fwrite($myfile, $txt);
+	  fclose($myfile);
+
     return self::$styles;
   }
 }
