605297fea04fa8db963cb3591e026eac2195c6fa
Nabi <artsanaat@gmail.com>
2022-12-04 10:45:56 +0300

    Fixed and Delete unnecessary codes.

--- src/RegisterElementorWidgets.php
+++ src/RegisterElementorWidgets.php
@@ -7,6 +7,7 @@
  */
 
 namespace WebkimaElements;
+
 use WebkimaElements\DynamicAssets;
 
 class RegisterElementorWidgets {
@@ -15,35 +16,25 @@ class RegisterElementorWidgets {
 
 	public static function init(): void {
 
-		if ( Base::isOptionActivated( 'we_el_widgets' ) ) {
-			add_action( 'elementor/widgets/register', __CLASS__ . '::registerElementorWidgets' );
-			add_action( 'elementor/elements/categories_registered', __CLASS__ . '::addElementorWidgetCategory' );
-			static::dynamicStyles();
+		if (Base::isOptionActivated('we_el_widgets')) {
+			add_action('elementor/widgets/register', __CLASS__ . '::registerElementorWidgets');
+			add_action('elementor/elements/categories_registered', __CLASS__ . '::addElementorWidgetCategory');
 		}
 
 	}
 
-	public static function registerElementorWidgets( $widgets_manager ): void {
-		if ( Base::isOptionActivated( 'we_el_widget_mobile_menu' ) ) {
+	public static function registerElementorWidgets($widgets_manager): void {
+		if (Base::isOptionActivated('we_el_widget_mobile_menu')) {
 			require_once static::$widgets_path . 'WebkimaELMobileMenu.php';
-			$widgets_manager->register( new \Webkima_EL_Mobile_Menu() );
+			$widgets_manager->register(new \Webkima_EL_Mobile_Menu());
 		}
 	}
 
-	public static function addElementorWidgetCategory( $elements_manager ): void {
-		$elements_manager->add_category( 'webkima-elements', [
-			'title' => esc_html__( 'Webkima Elements', 'webkima-elements' ),
+	public static function addElementorWidgetCategory($elements_manager): void {
+		$elements_manager->add_category('webkima-elements', [
+			'title' => esc_html__('Webkima Elements', 'webkima-elements'),
 			'icon'  => 'fa fa-plug',
-		] );
-	}
-
-	public static function dynamicStyles(): void {
-		if ( Base::isOptionActivated( 'we_el_widget_mobile_menu' ) ) {
-//			DynamicAssets::$styles[] = WEBKIMA_ELEMENTS_WIDGET_CSS_PATH . 'mobile-menu.css';
-		}
-		if ( Base::isOptionActivated( 'we_el_widget_another_one' ) ) {
-//			DynamicAssets::$styles[] = WEBKIMA_ELEMENTS_WIDGET_CSS_PATH . 'another-widget.css';
-		}
+		]);
 	}
 
 }
