c0fe06baaf7817856fb4a975a9d063d503a99db5
Nabi <artsanaat@gmail.com>
2022-12-24 16:24:23 +0300

    Added WordPress and PHP version notices.

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,39 +13,83 @@ use WebkimaElements\Base;
 
 class Plugin {
 
-    const PERFIX = 'webkima-elements';
+  const PERFIX = 'webkima-elements';
 
-    const L10N = self::PERFIX;
+  const L10N = self::PERFIX;
 
-    /**
-     * Registers pre init hooks.
-     *
-     * @since  1.0.0
-     * @return void
-     */
-    public static function pre_init(): void {
-
+  /**
+   * Registers on plugin loaded.
+   *
+   * @return void
+   * @since  1.7.0
+   */
+  public static function onPluginLoaded(): void {
+    self::loadTextDomain();
+    if (!Base::hasSatisfiedDependencies()) {
+      add_action('admin_notices', __CLASS__ . '::renderDependenciesNotice');
     }
+  }
 
-    /**
-     * Registers init hooks.
-     *
-     * @since  1.0.0
-     * @return void
-     */
-    public static function init(): void {
-	    Enqueue::init();
-	    RegisterElementorWidgets::init();
-	    DynamicAssets::init();
+  /**
+   * Notify about plugin dependency
+   *
+   * @return void
+   * @since      1.7.0
+   */
+  public static function renderDependenciesNotice(): void {
+    $user_id = get_current_user_id();
+    foreach (Base::getDependencyErrors() as $key => $message) {
+      if (!get_user_meta($user_id, 'webkima_elements_notice_dismissed_' . $key)) {
+        printf(
+          '<div class="error"><p>%1$s</p><a href="?webkima-elements-dismissed-%2$s">%3$s</a></div>',
+          $message,
+          $key,
+          __('بستن', 'webkima-elements')
+        );
+      }
     }
+  }
 
-    /**
-     * Registers and loads text domains.
-     *
-     * @since  1.0.0
-     * @return void
-     */
-    public static function loadTextDomain(): void {
-        load_plugin_textdomain('webkima-elements', false, 'webkima-elements/languages/');
+  public static function adminNoticeDismissed(): void {
+    $user_id = get_current_user_id();
+    foreach (Base::getDependencyErrors() as $key => $message) {
+      if (isset($_GET['webkima-elements-dismissed-' . $key])) {
+        add_user_meta($user_id, 'webkima_elements_notice_dismissed_' . $key, 'true', true);
+      }
     }
+  }
+
+  /**
+   * Registers pre init hooks.
+   *
+   * @return void
+   * @since  1.0.0
+   */
+  public static function pre_init(): void {
+
+  }
+
+  /**
+   * Registers init hooks.
+   *
+   * @return void
+   * @since  1.0.0
+   */
+  public static function init(): void {
+    self::adminNoticeDismissed();
+    Enqueue::init();
+    RegisterElementorWidgets::init();
+    DynamicAssets::init();
+  }
+
+  /**
+   * Registers and loads text domains.
+   *
+   * @return void
+   * @since  1.0.0
+   */
+  public static function loadTextDomain(): void {
+    load_plugin_textdomain('webkima-elements', false, 'webkima-elements/languages/');
+  }
+
 }
--- webkima-elements.php
+++ webkima-elements.php
@@ -9,6 +9,8 @@
  * Plugin URI: https://wordpress.org/plugins/webkima-elements/
  * Description: A very light plugin for Persianization and installation of Persian fonts on the free and Pro Elementor plugin
  * Version: 1.6.0
+ * Requires at least: 8.7
+ * Requires PHP: 7.5
  * Author: Webkima Academy
  * Author URI: https://webkima.com/
  * Text Domain: webkima-elements
@@ -24,42 +26,41 @@ use WebkimaElements\Base;
 
 // Exit if accessed directly.
 if (!defined('ABSPATH')) {
-    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-    exit;
+	header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+	exit;
 }
-
-define('WEBKIMA_ELEMENTS_PATH', plugin_dir_path( __FILE__ ));
-define('WEBKIMA_ELEMENTS_WIDGET_CSS_PATH', WEBKIMA_ELEMENTS_PATH . 'assets/dist/css/');
-define('WEBKIMA_ELEMENTS_WIDGET_JS_PATH', WEBKIMA_ELEMENTS_PATH . 'assets/dist/js/');
-define('WEBKIMA_ELEMENTS_URL', plugin_dir_url( __FILE__ ));
-define('WEBKIMA_ELEMENTS_ASSETS_URL', WEBKIMA_ELEMENTS_URL . 'assets/');
-define("WEBKIMA_ELEMENTS_VER", "1.5.0");
 
 /**
  * Loads PSR-4-style plugin classes.
  *
- * @since  1.0.0
  * @return void
+ * @since  1.0.0
  */
 function classloader($class): void {
-    static $ns_offset;
-    if (str_starts_with($class, __NAMESPACE__ . '\\')) {
-        if ($ns_offset === NULL) {
-            $ns_offset = strlen(__NAMESPACE__) + 1;
-        }
-        include __DIR__ . '/src/' . strtr(substr($class, $ns_offset), '\\', '/') . '.php';
-    }
+	static $ns_offset;
+	if (str_starts_with($class, __NAMESPACE__ . '\\')) {
+		if ($ns_offset === null) {
+			$ns_offset = strlen(__NAMESPACE__) + 1;
+		}
+		include __DIR__ . '/src/' . strtr(substr($class, $ns_offset), '\\', '/') . '.php';
+	}
 }
-
 spl_autoload_register(__NAMESPACE__ . '\classloader');
 
+define('WEBKIMA_ELEMENTS_PATH', plugin_dir_path(__FILE__));
+define('WEBKIMA_ELEMENTS_MAIN_FILE_PATH', WEBKIMA_ELEMENTS_PATH . basename(__FILE__));
+define('WEBKIMA_ELEMENTS_WIDGET_CSS_PATH', WEBKIMA_ELEMENTS_PATH . 'assets/dist/css/');
+define('WEBKIMA_ELEMENTS_WIDGET_JS_PATH', WEBKIMA_ELEMENTS_PATH . 'assets/dist/js/');
+define('WEBKIMA_ELEMENTS_URL', plugin_dir_url(__FILE__));
+define('WEBKIMA_ELEMENTS_ASSETS_URL', WEBKIMA_ELEMENTS_URL . 'assets/');
+define("WEBKIMA_ELEMENTS_VER", Base::getPluginVersion());
 
 register_activation_hook(__FILE__, __NAMESPACE__ . '\Base::activate');
 register_deactivation_hook(__FILE__, __NAMESPACE__ . '\Base::deactivate');
 register_uninstall_hook(__FILE__, __NAMESPACE__ . '\Base::uninstall');
 
 add_action("csf_init", __NAMESPACE__ . '\Plugin::loadTextDomain');
-add_action('plugins_loaded', __NAMESPACE__ . '\Plugin::loadTextDomain');
+add_action('plugins_loaded', __NAMESPACE__ . '\Plugin::onPluginLoaded');
 
 Base::register();
 add_action('init', __NAMESPACE__ . '\Plugin::pre_init', 0);
